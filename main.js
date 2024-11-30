60d3a84eede5da9a07b28d623b0f9eea05392614
CharlesMcGinn <58827376+CharlesMcGinn@users.noreply.github.com>
2024-11-29 23:54:58 -0500

    Update main.js to include links for GLTFloader and OrbitControls imports

--- main.js
+++ main.js
@@ -5,10 +5,10 @@
 //import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.168.0/examples/jsm/controls/OrbitControls.js';
 import gsap from 'https://cdn.skypack.dev/gsap';
 import * as THREE from './example/three.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'; // for loading models
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.171.0/examples/jsm/loaders/GLTFLoader.js'; // for loading models
 import * as CANNON from 'cannon-es'; // npm install cannon-es --save-dev
 import CannonDebugger from 'cannon-es-debugger'; // npm install cannon-es-debugger
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls' // moving object around with mouse
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.171.0/examples/jsm/controls/OrbitControls.js' // moving object around with mouse
 
 const raycaster = new THREE.Raycaster(); // for mouse move tracking (points at scene from mouse, it knows if its hitting object)
 const scene = new THREE.Scene(); // creating scene
